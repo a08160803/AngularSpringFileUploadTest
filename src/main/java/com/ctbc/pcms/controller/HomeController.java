5afa7f7e1ffcf555c659c5a35823f6e912003219
enzo <a08160803@gmail.com>
2018-06-21 23:10:00 +0800

    test hibernate max

--- src/main/java/com/ctbc/pcms/controller/HomeController.java
+++ src/main/java/com/ctbc/pcms/controller/HomeController.java
@@ -1,18 +1,26 @@
 package com.ctbc.pcms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ctbc.pcms.model.dao.FM001Dao;
 
 @Controller
 public class HomeController {
 
-	public HomeController() {
-		System.out.println("======== HomeController ===========");
-	}
-
-	@RequestMapping(value = "/")
-	public String toIndex() {
+	@Autowired
+	private FM001Dao fm001Dao;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		Integer max = fm001Dao.selectMax();
+		fm001Dao.insert(max);
+		max = fm001Dao.selectMax();
+		fm001Dao.insert2(max);
+		fm001Dao.selectMax();
 		return "index";
 	}
-
+	
 }
--- src/main/java/com/ctbc/pcms/model/dao/FM001Dao.java
+++ src/main/java/com/ctbc/pcms/model/dao/FM001Dao.java
@@ -1,11 +1,13 @@
 package com.ctbc.pcms.model.dao;
 
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.ctbc.pcms.model.vo.PCMS_BUSINESS_SET_VO;
+import com.ctbc.pcms.model.vo.PCMS_FLOW_MAIN_VO;
 
 @Repository
 public class FM001Dao {
@@ -13,14 +15,44 @@ public class FM001Dao {
 	@Autowired
 	private SessionFactory sessionFactory;
 	
-	public void insert() {
+	@Transactional
+	public void insert(Integer max) {
 		Session session = sessionFactory.getCurrentSession();
 		session.beginTransaction();
-		PCMS_BUSINESS_SET_VO pcmsBusinessSetVO = new PCMS_BUSINESS_SET_VO();
-		pcmsBusinessSetVO.setBusinessId("003");
-		pcmsBusinessSetVO.setBusinessName("幹你老師3");
-		session.save(pcmsBusinessSetVO);
+		PCMS_FLOW_MAIN_VO pcmsFlowMainVO = new PCMS_FLOW_MAIN_VO();
+		pcmsFlowMainVO.setCustomerName("AAAA");
+		pcmsFlowMainVO.setBusinessSeq(max+1);
+		session.save(pcmsFlowMainVO);
 		session.getTransaction().commit();
+	}
+	
+	@Transactional
+	public void insert2(Integer max) {
+		Session session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		PCMS_FLOW_MAIN_VO pcmsFlowMainVO = new PCMS_FLOW_MAIN_VO();
+		pcmsFlowMainVO.setCustomerName("BBBB");
+		pcmsFlowMainVO.setBusinessSeq(max+1);
+		session.save(pcmsFlowMainVO);
+		session.getTransaction().commit();
+	}
+	
+	public Integer selectMax() {
+		Session session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		Integer max;
+		
+		SQLQuery query = session.createSQLQuery("SELECT MAX(BUSINESS_SEQ) FROM PCMS_FLOW_MAIN");
+		if (query.uniqueResult() == null) {
+			max = 0;
+		} else {
+			max = (Integer) query.uniqueResult();
+		}
+		System.out.println("max >>>>>>>> " + max);
+		
+		session.getTransaction().commit();
+		
+		return max;
 	}
 	
 }
